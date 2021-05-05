181bf016e6319803802cb46646065da7c7170762
sanketb412 <sanketbagde38@gmail.com>
2021-05-05 15:12:53 +0530

    Added: code of Refatored changed the condition from switch_case to If else

--- src/Game/TicTacToe.java
+++ src/Game/TicTacToe.java
@@ -6,7 +6,6 @@ public class TicTacToe {
 
 	// creating Method1
 	private char[][] creatingBoard() {
-		@SuppressWarnings("unused")
 		// creating a board for empty
 		char board[][] = new char[3][3];
 		for (int i = 1; i < 3; i++) {
@@ -19,20 +18,19 @@ public class TicTacToe {
 	}
 
 	// creating Method2
-	public void input() {
+	public void ChossingLetter() {
 		// Choosing X or O for Player
 		System.out.println("Choose 'X' or 'O' ");
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
-		String letterPerson = sc.nextLine(); // getting input from player
-		System.out.println("Player Mark: " + letterPerson);
-		int letterComputer = (int) (Math.random() * 10) % 2; // getting random X or O from Computer
-		switch (letterComputer) {
-		case 0:
-			System.out.println("\nComputer Mark: O ");
-			break;
-		case 1:
-			System.out.println("\nComputer Mark: X");
-			break;
+		char letterPerson = sc.next().charAt(0); // getting input from player
+
+		if (letterPerson == 'X') {
+			System.out.println("Player Mark: 'X' ");
+			System.out.println("\nComputer Mark: 'O' ");
+		} else {
+			System.out.println("Player Mark: 'O' ");
+			System.out.println("\nComputer Mark: 'X' ");
 		}
 	}
 
@@ -41,7 +39,7 @@ public class TicTacToe {
 		System.out.println("..###..Welcome to Tic-Tac-Toe game..###..");
 		TicTacToe game = new TicTacToe();
 		game.creatingBoard(); // Calling method for the creating board
-		game.input(); // Calling method for knowing the input
+		game.ChossingLetter(); // Calling method for knowing the input
 	}
 
 }
