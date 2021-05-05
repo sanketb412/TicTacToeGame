465aaa920a075a8d68ab4364f9cfe810481b99c8
sanketb412 <sanketbagde38@gmail.com>
2021-05-05 19:07:41 +0530

    Added: code for Displaying TicTacToe Board with specific cell

--- src/Game/TicTacToe.java
+++ src/Game/TicTacToe.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+	private char board[] = new char[10];
+
 	// creating Method1
-	private char[][] creatingBoard() {
-		// creating a board for empty
-		char board[][] = new char[3][3];
-		for (int i = 1; i < 3; i++) {
-			for (int j = 1; j < 3; j++) {
-				board[i][j] = ' ';
-				System.out.println("| " + board[i][j] + "| " + board[i][j] + "| " + board[i][j] + "| ");
-			}
-		}
-		return board;
+	private void creatingBoard() {
+		// creating a board with number inside for choosing
+		char position[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		for (int i = 1; i < 10; i++)
+			board[i] = position[i];
 	}
 
 	// creating Method2
-	public void ChossingLetter() {
+	public String showBoard() {
+		System.out.println("\n" + board[1] + "  | " + board[2] + "  | " + board[3]); // cell of 1st row
+		System.out.println("" + board[4] + "  | " + board[5] + "  | " + board[6]); // cell of 2nd row
+		System.out.println("" + board[7] + "  | " + board[8] + "  | " + board[9]); // cell of 3rd row
+		return "currentBoard";
+	}
+
+	// creating Method3
+	public void chossingLetter() {
 		// Choosing X or O for Player
-		System.out.println("Choose 'X' or 'O' ");
+		System.out.println("\nChoose 'X' or 'O' ");
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		char letterPerson = sc.next().charAt(0); // getting input from player
@@ -38,8 +43,8 @@ public class TicTacToe {
 		// TODO Auto-generated method stub
 		System.out.println("..###..Welcome to Tic-Tac-Toe game..###..");
 		TicTacToe game = new TicTacToe();
-		game.creatingBoard(); // Calling method for the creating board
-		game.ChossingLetter(); // Calling method for knowing the input
+		game.creatingBoard(); // Calling method for the creating board.
+		game.showBoard(); // Calling method of current board.
+		game.chossingLetter(); // Calling method for knowing the input.
 	}
-
 }
